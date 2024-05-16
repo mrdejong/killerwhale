94b8d386ec7152112bc2b09d0473976b5532d9d6
Alexander de Jong <alexander@dutchcaffeine.nl>
2024-05-16 20:20:01 +0200

    Add composer
    Time get composer up and running, we are going to need some 3rd party
    packages :)

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+require __DIR__."/../vendor/autoload.php";
 
 $path = array_key_exists('PATH_INFO', $_SERVER) ? substr($_SERVER['PATH_INFO'], 1) : '';
 $method = $_SERVER['REQUEST_METHOD'];
